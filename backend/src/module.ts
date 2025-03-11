d70adc25c12e35e872a13996d769a46869ef46d9
Reinder Nijhoff <reinder@mediamonks.com>
2025-03-11 11:04:24 +0100

    started with authentication

--- backend/src/module.ts
+++
@@ -1,3 +0,0 @@
-import { Elysia } from "elysia";
-
-export const plugin = new Elysia().get("/plugin", "From plugin");
--- frontend/src/apps/mail/src/main.tsx
+++ frontend/src/apps/mail/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import {createRouter, RouterProvider} from '@tanstack/react-router';
 import {routeTree} from './routeTree.gen';
+import "@/lib/auth/auth-client";
 
 // Set up a Router instance
 const router = createRouter({
