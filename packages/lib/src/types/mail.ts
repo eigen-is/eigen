d476532af158c299d630f80f7598271e5effcbd6
Reinder Nijhoff <reinder@eigen.is>
2025-03-12 22:50:15 +0100

    code-layout

--- packages/lib/src/types/mail.ts
+++ packages/lib/src/types/mail.ts
@@ -6,16 +6,16 @@
  * Email type representing an email message
  */
 export interface Email {
-  id: string;
-  read: boolean;
-  starred: boolean;
-  from: {
-    name: string;
-    email: string;
-  };
-  subject: string;
-  preview: string;
-  hasAttachment: boolean;
-  date: string;
-  important?: boolean;
+    id: string;
+    read: boolean;
+    starred: boolean;
+    from: {
+        name: string;
+        email: string;
+    };
+    subject: string;
+    preview: string;
+    hasAttachment: boolean;
+    date: string;
+    important?: boolean;
 }
