e34603292c9796224b66c52ab815837a99757970
Reinder Nijhoff <reinder@eigen.is>
2025-03-16 17:46:21 +0100

    verplaatsen topbar, zodat ie gedeeld is

--- apps/contacts/src/routes/__root.tsx
+++ apps/contacts/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import {createRootRouteWithContext, Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 import {AuthContextType} from "@workspace/lib/auth/auth-context.tsx";
-import {Topbar} from "../../components/layout/topbar.tsx";
+import {Topbar} from "@workspace/ui/components/layout/topbar";
 
 const appName = 'contacts';
 
@@ -13,7 +13,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
     component: () => (
         <>
             <div className="flex flex-col h-dvh">
-                <Topbar appName={appName}/>
+                <Topbar appName={appName} rootRoute={Route}/>
                 <Outlet/>
             </div>
             <TanStackRouterDevtools position="bottom-right"/>
