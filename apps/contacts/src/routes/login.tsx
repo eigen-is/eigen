5367c81ee7c32a55ee117a593ab85b03bc10d625
Reinder Nijhoff <reinder@eigen.is>
2025-03-16 17:00:42 +0100

    more fixes

--- apps/contacts/src/routes/login.tsx
+++ apps/contacts/src/routes/login.tsx
@@ -5,7 +5,7 @@ import {LoginPage} from "@workspace/ui/components/layout/loginpage";
 const fallback = '/all';
 
 export const Route = createFileRoute('/login')({
-    component: LoginPage,
+    component: () => <LoginPage appName="contacts" />,
     validateSearch: z.object({
         redirect: z.string().optional().catch(''),
     }),
