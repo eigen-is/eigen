a6129603985a9a11caad04f961027c218392f166
Reinder Nijhoff <reinder@eigen.is>
2025-03-24 14:03:03 +0100

    wat een geklooi

--- packages/lib/src/lib/api.ts
+++ packages/lib/src/lib/api.ts
@@ -1,7 +1,7 @@
 import { edenTreaty } from '@elysiajs/eden';
 import type { app } from "@apps/api-server";
 
-export const api = edenTreaty<app>('http://localhost:8000', {
+export const api = edenTreaty<app>('https://eigen.is:8000', {
     $fetch: {
         credentials: 'include'
     }
