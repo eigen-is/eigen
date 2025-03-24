707a2f549617c9a8755ffd5fc36a40336182940d
Reinder Nijhoff <reinder@eigen.is>
2025-03-24 14:48:13 +0100

    local fixes, maar nu werkt t niet meer op eigen.is

--- packages/lib/src/lib/api.ts
+++ packages/lib/src/lib/api.ts
@@ -2,7 +2,7 @@ import { edenTreaty } from '@elysiajs/eden';
 import type { app } from "@apps/api-server";
 
 // export const api = edenTreaty<app>('https://eigen.is:8000', {
-export const api = edenTreaty<app>(import.meta.env.NODE_ENV === 'developtment' ? 'http://localhost:8000' : 'https://eigen.is:8000', {
+export const api = edenTreaty<app>(import.meta.env.NODE_ENV !== 'production' ? 'http://localhost:8000' : 'https://eigen.is:8000', {
     $fetch: {
         credentials: 'include'
     }
