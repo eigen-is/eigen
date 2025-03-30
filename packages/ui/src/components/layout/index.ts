67d579e644d4b7d11c6bddf4fbbd4656586a67db
Reinder Nijhoff <reinder@eigen.is>
2025-03-30 19:25:32 +0200

    eigen-app component

---
+++ packages/ui/src/components/layout/index.ts
@@ -0,0 +1,7 @@
+// Export the EigenApp component
+export { EigenApp } from "./eigen-app"
+
+// Export other layout components
+export { AppLogo } from "./app-logo"
+// AppSidebar export removed - add back if this component exists
+export { Topbar } from "./topbar"
