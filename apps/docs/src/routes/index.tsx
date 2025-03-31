cf7d971d99dc409cc7031e03b65598da6b20f286
Reinder Nijhoff <reinder@eigen.is>
2025-03-31 13:20:02 +0200

    small fixes

--- apps/docs/src/routes/index.tsx
+++ apps/docs/src/routes/index.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/')({
 function HomeComponent() {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-            <Shovel className="w-24 h-24 text-teal-600 mb-4" />
+            <Shovel className="w-24 h-24 text-app mb-4" />
             <h1 className="text-3xl text-gray-800 select-none">under construction</h1>
         </div>
     );
