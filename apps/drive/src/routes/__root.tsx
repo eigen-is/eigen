113f5302c6e1a1c602961fede748b68bf5ef131f
reindernijhoff <reinder@infi.nl>
2025-04-02 09:53:55 +0200

    Fix drive hooks

--- apps/drive/src/routes/__root.tsx
+++ apps/drive/src/routes/__root.tsx
@@ -3,7 +3,7 @@ import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 import {AuthContextType} from "@workspace/lib/auth/auth-context.tsx";
 import {Topbar} from "@workspace/ui/components/layout/topbar";
 import {createContext, useState} from 'react';
-import {useMediaQuery} from '../hooks/use-media-query';
+import {useMediaQuery} from '@workspace/lib/drive';
 
 const appName = 'drive';
 
