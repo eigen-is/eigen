36b3d1c4deb12037694e0702520894dc3216b91c
Reinder Nijhoff <reinder@mediamonks.com>
2025-04-03 14:45:14 +0200

    fixes

---
+++ apps/space/src/routes/_auth.user.tsx
@@ -0,0 +1,27 @@
+import {createFileRoute} from '@tanstack/react-router'
+import { useAuth } from '@workspace/lib/auth/auth-context.js';
+import { useContacts } from '@workspace/lib/contacts';
+
+export const Route = createFileRoute('/_auth/user')({
+    component: RouteComponent,
+})
+
+function RouteComponent() {
+    const navigate = Route.useNavigate();
+    const { data: contacts = [], isLoading: contactsLoading } = useContacts();
+    const auth = useAuth();
+
+    if (contacts) {
+        // find yourself!
+        const yourself = contacts.find(contact => contact.eigenId === auth.user?.id);
+        if (yourself) {
+            navigate({
+                href: `${import.meta.env.VITE_APP_CONTACTS_URL}/book/all?contactId=${yourself.id}`,
+                reloadDocument: true,
+                replace: true,
+            });
+            return null;
+        }
+    }
+    return null;
+}
