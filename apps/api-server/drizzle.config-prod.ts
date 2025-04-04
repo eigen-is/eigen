6321a43c87e2515ed951445805d4e29533e5cdb8
Reinder Nijhoff <reinder@mediamonks.com>
2025-04-04 13:22:03 +0200

    fixes for live deploy

--- apps/api-server/drizzle.config-prod.ts
+++ apps/api-server/drizzle.config-prod.ts
@@ -2,5 +2,5 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
     dialect: "sqlite",
     schema: "./auth-schema.ts",
-    url: "~/eigen/api/data/users3.db",
+    url: "../../../../eigen/api/data/users3.db",
 });
