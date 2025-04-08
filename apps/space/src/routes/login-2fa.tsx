bb2f88f96c85420320e4151a7b7afd45023309b3
Mark <mark@mediamonks.com>
2025-04-08 16:01:37 +0200

    Fix actual 2fa flow (and rename to 2fa)

--- apps/space/src/routes/login-2fa.tsx
+++ apps/space/src/routes/login-2fa.tsx
@@ -1,17 +1,17 @@
 import LoginFa2Page from '../components/space/login-fa2';
-import { createFileRoute, redirect } from '@tanstack/react-router';
+import { createFileRoute, redirect, useRouter } from '@tanstack/react-router';
 import { z } from 'zod';
 import { authClient } from '@workspace/lib/auth';
 
-const fallback = '/';
+const fallback = '/login';
 
-export const Route = createFileRoute('/login-fa2')({
+export const Route = createFileRoute('/login-2fa')({
     component: LoginFa2Page,
     validateSearch: z.object({
         redirect: z.string().optional().catch(''),
     }),
     beforeLoad: async ({context, search}) => {
-        if (context.auth.isAuthenticated) {
+        if (context.auth.isAuthenticated ) {
             throw redirect({to: search.redirect || fallback})
         }
     },
