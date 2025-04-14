c1def70bb34a6248e3c9f9f1803f398044f71c26
Reinder Nijhoff <reinder@eigen.is>
2025-04-14 09:33:06 +0200

    fix drive interface for docs

--- apps/docs/src/routes/__root.tsx
+++ apps/docs/src/routes/__root.tsx
@@ -2,27 +2,42 @@ import {createRootRouteWithContext, Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 import {AuthContextType} from "@workspace/lib/auth/auth-context.tsx";
 import {Topbar} from "@workspace/ui/components/layout/topbar";
+import {createContext, useState} from 'react';
+import {useIsMobile} from "@workspace/lib/media";
+
+// Create sidebar context to manage sidebar visibility
+export const SidebarContext = createContext<{
+    sidebarOpen: boolean;
+    setSidebarOpen: (open: boolean) => void;
+}>({
+    sidebarOpen: false,
+    setSidebarOpen: () => {
+    },
+});
 
 interface MyRouterContext {
     auth: AuthContextType
 }
 
+
 export const Route = createRootRouteWithContext<MyRouterContext>()({
     component: () => {
-        const isMobile = false;
+        const [sidebarOpen, setSidebarOpen] = useState(false);
+        const isMobile = useIsMobile();
 
         return (
             <>
-                <div className="flex flex-col h-dvh">
-                    <Topbar
-                        rootRoute={Route}
-                        showMobileMenu={isMobile}
-                        onMobileMenuClick={() => {
-                        }}
-                        isMobile={isMobile}
-                    />
-                    <Outlet/>
-                </div>
+                <SidebarContext.Provider value={{sidebarOpen, setSidebarOpen}}>
+                    <div className="flex flex-col h-dvh">
+                        <Topbar
+                            rootRoute={Route}
+                            showMobileMenu={isMobile}
+                            onMobileMenuClick={() => setSidebarOpen(true)}
+                            isMobile={isMobile}
+                        />
+                        <Outlet/>
+                    </div>
+                </SidebarContext.Provider>
                 <TanStackRouterDevtools position="bottom-right"/>
             </>)
     }
