844d4ebdc98ccffaf84b529fce4c2fb5a2b4ae46
Reinder Nijhoff <reinder@eigen.is>
2025-04-16 13:13:54 +0200

    space clean up

--- apps/space/src/routes/_auth.security.2fa.tsx
+++ apps/space/src/routes/_auth.security.2fa.tsx
@@ -74,9 +74,8 @@ function TwoFaComponent() {
     };
 
     return (
-
-        <div className="flex flex-col min-h-screen">
-            <div className="w-full max-w-3xl m-8">
+        <div className="flex flex-col min-h-screen m-8">
+            <div className="w-full max-w-3xl">
                 <h1 className="text-2xl font-semibold mb-6">Two-Factor Authentication</h1>
 
                 <TwoFactorSetup
