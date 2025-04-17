a54702021abb82a1bf23e3883ee1396ae5f5fbb4
Mark <mark@mediamonks.com>
2025-04-17 11:33:29 +0200

    view-mode in docs, uses `access.canWrite`

---
+++ packages/ui/src/components/layout/toolbar/DocumentModeButton.tsx
@@ -0,0 +1,33 @@
+import { Eye, Pencil } from 'lucide-react';
+import {Button} from "@workspace/ui/components/button";
+import {Tooltip, TooltipContent, TooltipTrigger,} from "@workspace/ui/components/tooltip";
+
+
+export interface DocumentModeButtonProps {
+    canWrite: boolean;
+}
+
+export const DocumentModeButton = ({
+  canWrite
+}: DocumentModeButtonProps) => {
+  return (
+    <>
+    <Tooltip>
+        <TooltipTrigger asChild>
+            <Button
+                variant="ghost"
+                onClick={() => {}}
+            >
+                {canWrite ? <Pencil className="h-4 w-4"/> : <Eye className="h-4 w-4"/>}
+                {canWrite ? 'Editing' : 'Read-only'}
+            </Button>
+        </TooltipTrigger>
+        <TooltipContent>{
+            canWrite ? 
+                'You can edit this document because you are Editor' :
+                'You can only read this document because you\'re set to Viewer'
+            }</TooltipContent>
+    </Tooltip>
+</>
+  );
+};
