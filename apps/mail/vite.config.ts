0219483efaabb8c34344f3c90eb9fcbdcaf72881
Reinder Nijhoff <reinder@mediamonks.com>
2025-04-17 10:35:13 +0200

    smaller builds?

--- apps/mail/vite.config.ts
+++ apps/mail/vite.config.ts
@@ -28,5 +28,38 @@ export default defineConfig({
     build: {
         outDir: './../../dist/mail',
         emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash][extname]',
+            },
+        },
+        minify: 'terser', // Use terser for advanced minification
+        terserOptions: {
+            compress: {
+              drop_console: true,
+              drop_debugger: true,
+              sequences: true,
+              properties: true,
+              dead_code: true,
+              conditionals: true,
+              evaluate: true,
+              booleans: true,
+              loops: true,
+              unused: true,
+              keep_fargs: false, // remove unused function args
+            },
+            mangle: {
+              toplevel: true,        // mangle top-level names
+              properties: true,
+            },
+            format: {
+              comments: false,
+              beautify: false,
+            },
+            module: true,
+        },
+        sourcemap: false, // Disable sourcemaps for minimal size
     }
 });
