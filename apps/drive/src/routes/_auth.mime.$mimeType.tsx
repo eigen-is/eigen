80b072f316b16b0a7d48acb3b2465162db8208cb
Reinder Nijhoff <reinder@eigen.is>
2025-04-22 10:22:04 +0200

    open documents in same tab

--- apps/drive/src/routes/_auth.mime.$mimeType.tsx
+++ apps/drive/src/routes/_auth.mime.$mimeType.tsx
@@ -74,10 +74,10 @@ function DriveRoute() {
             });
         } else if (path.type === 'doc') {
             const url = `${import.meta.env.VITE_APP_DOCS_URL}/doc/${path.ownerId}/${path.id}`;
-            window.open(url, '_blank');
+            document.location.href = url;
         } else if (path.type === 'stickies') {
             const url = `${import.meta.env.VITE_APP_STICKIES_URL}/board/${path.ownerId}/${path.id}`;
-            window.open(url, '_blank');
+            document.location.href = url;
         } else if (fileMimeType.startsWith("image/") || fileMimeType.startsWith("video/")) {
             const url = `${import.meta.env.VITE_API_HOST}/drive/embed/${path.ownerId}/${path.id}/${path.name}`;
             setPreview({url, mimeType: fileMimeType});
