f36d5bcce5fc30d475bebe59bc4a37084180a653
Reinder Nijhoff <reinder@eigen.is>
2026-02-28 15:54:11 +0100

    Refactor auth.ts and related files to improve user organization handling and database access

--- apps/api/src/lib/user/user.ts
+++ apps/api/src/lib/user/user.ts
@@ -1,31 +1,19 @@
-import {drizzle} from "drizzle-orm/bun-sqlite";
-import {account, session, user, verification} from '../../../auth-schema.ts';
+import {user} from '../../../auth-schema.ts';
 import {eq} from "drizzle-orm";
-import {getServerDataPath} from "../config/paths.ts";
 import type {User} from "better-auth/types";
-
-function getUserDb() {
-    return drizzle(getServerDataPath('users3.db'), {
-        schema: {
-            user,
-            session,
-            verification,
-            account,
-        },
-    });
-}
+import {getAuthDrizzleDb} from "../auth/auth.ts";
 
 export async function getUserByEmail(email: string) {
-    const db = getUserDb();
+    const db = getAuthDrizzleDb();
     return await db.select().from(user).where(eq(user.email, email.toLocaleLowerCase())).get() as User | null;
 }
 
 export async function getUserById(id: string) {
-    const db = getUserDb();
+    const db = getAuthDrizzleDb();
     return await db.select().from(user).where(eq(user.id, id)).get() as User | null;
 }
 
 export async function updateUser(me: User, name: string, image: string) {
-    const db = getUserDb();
+    const db = getAuthDrizzleDb();
     return await db.update(user).set({name, image}).where(eq(user.id, me.id));
 }
