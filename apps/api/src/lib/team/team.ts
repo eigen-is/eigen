84e9f7c1bec0ffb145da5e1e007c9eabf0b5b91d
Reinder Nijhoff <reinder@eigen.is>
2026-02-28 15:54:11 +0100

    Refactor auth.ts and related files to improve user organization handling and database access

--- apps/api/src/lib/team/team.ts
+++ apps/api/src/lib/team/team.ts
@@ -1,24 +1,9 @@
 import {eq} from "drizzle-orm";
-import {drizzle} from "drizzle-orm/bun-sqlite";
-import {getServerDataPath} from "../config/paths.ts";
-import {account, member, organization, session, team, teamMember, user, verification} from "../../../auth-schema.ts";
+import {team} from "../../../auth-schema.ts";
+import {getAuthDrizzleDb} from "../auth/auth.ts";
 
-function getTeamDb() {
-    return drizzle(getServerDataPath('users3.db'), {
-        schema: {
-            user,
-            session,
-            verification,
-            account,
-            organization,
-            team,
-            member,
-            teamMember
-        },
-    });
-}
 
 export async function getTeamExists(teamId: string) {
-    const db = getTeamDb();
-    return await db.select().from(team).where(eq(team.id, teamId)).get() !== undefined;
+    const db = getAuthDrizzleDb();
+    return await db.select({id: team.id}).from(team).where(eq(team.id, teamId)).get() !== undefined;
 }
