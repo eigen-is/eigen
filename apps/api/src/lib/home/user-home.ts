9f56300d9cc60e968f46aa0a1c978b798d8100f8
Reinder Nijhoff <reinder@eigen.is>
2026-02-28 15:43:33 +0100

    Add OrgHome and UserHome classes with team and user management functionality

---
+++ apps/api/src/lib/home/user-home.ts
@@ -0,0 +1,17 @@
+import {Home} from "./home.ts";
+import type {User} from "better-auth/types";
+import {getUserHomePath} from "../config/paths.ts";
+import {LocalStorage} from "../storage";
+import {Contacts} from "../contacts/contacts.ts";
+import Maildir from "../mail/maildir.ts";
+
+export class UserHome extends Home {
+    constructor(user: User, cleanUp?: () => void) {
+        super(user, cleanUp);
+        this.user = user;
+        this.homeDir = getUserHomePath(user.id);
+        this.fs = new LocalStorage(this.homeDir);
+        this.contacts = new Contacts(this);
+        this.mail = new Maildir(this);
+    }
+}
