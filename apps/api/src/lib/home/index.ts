80b8a4dd9c9ffc401035da1226969e7b2cf00e48
Reinder Nijhoff <reinder@eigen.is>
2026-03-21 17:21:40 +0100

    feat: add atHome check before notifying users in share propagation

--- apps/api/src/lib/home/index.ts
+++ apps/api/src/lib/home/index.ts
@@ -2,4 +2,4 @@ export {Home} from './home';
 export {TeamHome} from './team-home';
 export {UserHome} from './user-home';
 export {OrgHome} from './org-home';
-export {getHome, cleanupHomeFactory} from './get-home';
+export {atHome, getHome, cleanupHomeFactory} from './get-home';
