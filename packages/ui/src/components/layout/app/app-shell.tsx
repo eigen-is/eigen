9fbb04769449483dcd440f4f7930729ef72730f7
Reinder Nijhoff <reinder@eigen.is>
2026-04-10 22:37:30 +0200

    fix(layout): update app shell and globals.css for improved height handling

--- packages/ui/src/components/layout/app/app-shell.tsx
+++ packages/ui/src/components/layout/app/app-shell.tsx
@@ -52,7 +52,7 @@ export function AppShell({
                 isTablet,
             }}
         >
-            <div className="flex flex-col min-h-dvh relative">
+            <div className="flex flex-col h-full">
                 <Topbar rootRoute={rootRoute} />
                 <div className="flex flex-1 w-full overflow-hidden">
                     {sidebar && !sidebarHidden && <SidebarContainer sidebar={sidebar} />}
