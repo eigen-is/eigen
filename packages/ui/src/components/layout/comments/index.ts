53a030a57a54d8ed1d689f60f6c137700fad4063
Reinder Nijhoff <reinder@eigen.is>
2026-05-17 15:20:41 +0200

    feat(comments): drop CommentDialog + CreateCommentDialog

--- packages/ui/src/components/layout/comments/index.ts
+++ packages/ui/src/components/layout/comments/index.ts
@@ -1,5 +1,3 @@
-export { CommentDialog } from './comment-dialog';
 export { useCreatedByMeta } from './comment-dialog-meta';
 export { CommentPanel } from './comment-panel';
 export { CommentThread } from './comment-thread';
-export { CreateCommentDialog } from './create-comment-dialog';
