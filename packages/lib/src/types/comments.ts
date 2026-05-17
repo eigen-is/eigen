5f5db2cebdb287b57a0fc53e99007a3f614e8d03
Reinder Nijhoff <reinder@eigen.is>
2026-05-17 11:50:07 +0200

    feat(comments): add CommentCard type

---
+++ packages/lib/src/types/comments.ts
@@ -0,0 +1,9 @@
+export type CommentCard = {
+    id: string;
+    title: string;
+    description: string;
+    color?: string;
+    chatName?: string;
+    creator?: string;
+    createdAt?: number;
+};
